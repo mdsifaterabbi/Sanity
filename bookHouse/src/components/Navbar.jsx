08ee9531714408389a2fbd29d10b4796810fd0ee
srbd911 <sr.bd.911@gmail.com>
2023-10-28 03:23:13 +0600

    search movie field added

--- bookHouse/src/components/Navbar.jsx
+++ bookHouse/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbar fixed top-0 w-full z-10" style={myStyle}>
+            <div className="navbar" style={myStyle}>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
