5b7db291effe319432a22e274ab6c15d32bb2fec
srbd911 <sr.bd.911@gmail.com>
2023-10-29 02:45:11 +0600

    slider added in home page

---
+++ bookHouse/src/components/MovieSlider.jsx
@@ -0,0 +1,55 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+
+import './styles.css'
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard, FreeMode } from 'swiper/modules';
+
+
+
+import pathan_1 from '../assets/pathan_1.jpg'
+
+
+const MovieSlider = () => {
+
+    return (
+
+        <>
+            <Swiper
+                mousewheel={true}
+                slidesPerView={3}
+                spaceBetween={30}
+                cssMode={true}
+                navigation={true}
+                pagination={{
+                    clickable: true,
+                }}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard, FreeMode]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+                <SwiperSlide><img src={pathan_1} alt="Movie" style={{ width: '300px', height: '300px', marginTop: '20px', borderRadius: '30px' }} /></SwiperSlide>
+
+
+
+            </Swiper>
+        </>
+
+    )
+}
+
+export default MovieSlider
--- bookHouse/src/pages/Home.jsx
+++ bookHouse/src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import MovieSlider from "../components/MovieSlider"
 import Layout from "../layout/Layout"
 
 const Home = () => {
     return (
         <>
             <Layout title={"Home"}>
-                <h1>Home</h1>
+                <MovieSlider />
             </Layout>
 
         </>
